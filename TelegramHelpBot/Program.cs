2e568816416d8fc76e73f0445e440a913b2f04ae
Антон Савич <antonyk99@gmail.com>
2023-05-31 13:40:44 +0400

    TelBot example

--- TelegramHelpBot/Program.cs
+++ TelegramHelpBot/Program.cs
@@ -1,100 +1,91 @@
-using Telegram.Bot.Polling;
-using HtmlAgilityPack;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Telegram.Bot;
-using Telegram.Bot.Types;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
-using System.Xml.Linq;
-using System.Xml;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
-internal class Program
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+var botClient = new TelegramBotClient("5935980402:AAEq6uZKACvGRSfnk9dqwvb0lq8rcky1y5s");
+
+using CancellationTokenSource cts = new();
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+ReceiverOptions receiverOptions = new()
 {
-    static ITelegramBotClient bot = new TelegramBotClient("5935980402:AAEq6uZKACvGRSfnk9dqwvb0lq8rcky1y5s");
+    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+};
 
-    private static async Task Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
 
-        Console.WriteLine("Bot was started!!!");
+var me = await botClient.GetMeAsync();
 
-        var cts = new CancellationTokenSource();
-        var cancellationToken = cts.Token;
-        var receiverOptions = new ReceiverOptions
-        {
-            AllowedUpdates = { }, // receive all update types
-        };
-        bot.StartReceiving(
-            HandleUpdateAsync,
-            HandleErrorAsync,
-            receiverOptions,
-            cancellationToken
-        );
-        Console.ReadLine();
-        Console.WriteLine("Bot was stopped!!!");
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
 
-        // Add services to the container.
-        builder.Services.AddRazorPages();
+// Send cancellation request to stop bot
+cts.Cancel();
 
-        var app = builder.Build();
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Message is not { } message)
+        return;
+    // Only process text messages
+    if (message.Text is not { } messageText)
+        return;
 
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
+    var chatId = message.Chat.Id;
 
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
 
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapRazorPages();
-
-        app.Run();
-    }
-
-    public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-    {
-        // Некоторые действия
-        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
-    }
-
-    public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-    {
-        // Некоторые действия
-        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
-        if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
-        {
-            var message = update.Message;
-            if (message.Text.ToLower() == "/start")
-            {
-                HtmlWeb web = new HtmlWeb();
-
-                web.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36";
-                HtmlDocument doc = web.Load("https://www.bloomberg.com/quote/USDGEL:CUR");
-                HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//span[contains(@class, 'priceText__0550103750')]");
-
-                if (nodes != null)
-                {
-                    foreach (HtmlNode node in nodes)
-                        await botClient.SendTextMessageAsync(message.Chat, node.InnerHtml + "GEL");
-                }
-
-                return;
-            }
-            if (message.Text.ToLower() == "/who")
-            {
-                await botClient.SendTextMessageAsync(message.Chat, "Ирка казюля");
-                return;
-            }
-            await botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
-        }
-    }
+    // Echo received message text
+    Message sentMessage = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: "You said:\n" + messageText,
+        cancellationToken: cancellationToken);
 }
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
